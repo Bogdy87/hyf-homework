4d617fae5561a67ce44e7167d7f95e35634d1dae
Bogdy87 <dobinda.bogdan@yahoo.com>
2024-04-22 13:54:35 +0200

    class exercises done

---
+++ javascript/javascript1/week1/pizza-exercise/pizza.js
@@ -0,0 +1,34 @@
+// console.log("I love pizza");
+
+// let myFavouritePizza = "Capriciosa.";
+// let regularPriceOfPizza = 156; // KR
+// let familySizeMultiplier = 2; // only applicable if the pizza is family size
+
+// let amountOfPizzas = 10;
+// let isFamilySize = true; // set to true if you want to order a family size pizza
+
+// // calculate the price of the pizza based on the size
+// let priceOfPizza = isFamilySize ? (regularPriceOfPizza * familySizeMultiplier) : regularPriceOfPizza;
+
+// let totalPrice = amountOfPizzas * priceOfPizza;
+
+// console.log("New pizza order:", myFavouritePizza, "The price of the pizza is:", priceOfPizza, "Total price for", amountOfPizzas, isFamilySize ? "family size pizzas" : "regular pizzas", ":", totalPrice);
+
+
+console.log("I love pizza");
+let myFavouritePizza = "Capriciosa."
+let regularPriceOfPizza= 100;
+let familySizeMultiplyer = 2;
+
+let amountOfPizza = 10;
+let isFamilySize = true;
+
+
+
+
+
+let finalPrice = isFamilySize ?(regularPriceOfPizza * familySizeMultiplyer) : regularPriceOfPizza ;
+
+let totalPrice = amountOfPizza * finalPrice 
+
+console.log("New pizza order:" ,amountOfPizza , isFamilySize ? "family size pizzas" : "regular pizzas",":", myFavouritePizza ,"Total cost for the order is:" , totalPrice);
